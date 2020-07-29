958d230265a3fe7d628bb99df9e7669f02e53487
Tung Nguyen <tung@vn2rap.com>
2020-07-30 03:22:53 +0700

    feat: stage #1 - a needle in the hay
    implement features:
    - load directory and find data into memory
    - search names using linear search algorithm
    - calculate time taken and print formatted result
    
    download links:
    - `directory.txt`: https://stepik.org/media/attachments/lesson/197761/directory.txt
    - `find.txt`: https://stepik.org/media/attachments/lesson/197761/find.txt

--- Phone Book/task/src/phonebook/Main.kt
+++ Phone Book/task/src/phonebook/Main.kt
@@ -1,5 +1,28 @@
 package phonebook
 
+import java.io.File
+import java.time.Duration
+import java.time.Instant.now
+
 fun main() {
-    println("Hello, World!")
+    // Load data into memory
+    val directory = File("D:/directory.txt").readLines()
+    val names = File("D:/find.txt").readLines()
+    var foundCount = 0
+    // Start linear search
+    println("Start searching...")
+    val start = now()
+    for (name in names) {
+        for (line in directory) {
+            if (line.endsWith(name)) {
+                foundCount++
+                break
+            }
+        }
+    }
+    val end = now()
+    // Calculate time taken
+    val d = Duration.between(start, end)
+    val timeTaken = "${d.toMinutesPart()} min. ${d.toSecondsPart()} sec. ${d.toMillisPart()} ms."
+    println("Found $foundCount / ${names.size} entries. Time taken: $timeTaken")
 }
